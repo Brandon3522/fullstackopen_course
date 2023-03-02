7384c1088d84d815ce403aad1beea98aef3fc2c8
Brandon3522 <77870751+Brandon3522@users.noreply.github.com>
2023-03-02 17:06:55 -0600

    Part 1 exercises complete

--- src/App.js
+++ src/App.js
@@ -75,14 +75,76 @@ const Button = ({ handleClick, text }) => {
 // ########### END COUNTER ###########
 
 // ########### PART 1 EXERCISE 1.6 - 1.14 ###########
-const DisplayFeedback = ({feedback, feedbackName}) => {
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <>
+      <tr>
+        <td>{text}:</td>
+        <td>{value}</td>
+      </tr>
+    </>
+  );
+};
+
+const Statistics = ({ good, bad, neutral }) => {
+  const total = good + bad + neutral;
+  const average = total / 3;
+  const positive = ((good / total) * 100).toFixed(2);
+
+  if (total === 0) {
+    return (
+      <>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <table>
+        <tbody>
+          <StatisticsLine text={'Good'} value={good} />
+          <StatisticsLine text={'Neutral'} value={neutral} />
+          <StatisticsLine text={'Bad'} value={bad} />
+          <StatisticsLine text={'Total'} value={total} />
+          <StatisticsLine text={'Average'} value={average} />
+          <StatisticsLine text={'Positive'} value={positive} />
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+const AnecdoteVotes = ({votes, selected, anecdotes}) => {
 	return (
 		<>
-			<p>{feedbackName}: {feedback}</p>
+			<p>{anecdotes[selected]}</p>
+			<p>has {votes[selected]} votes.</p>
 		</>
 	)
 }
 
+const MaxVotes = ({votes, anecdotes}) => {
+	let max = Math.max(...votes) // Get max
+	console.log(max)
+	let index = votes.indexOf(max) // Get index at max
+	let maxVotes = anecdotes[index] // Get anecdote with max votes
+
+	if (max < 1 || max === null || max === undefined) {
+		return (
+			<>
+				<p>No vote has been selected.</p>
+			</>
+		)
+	}
+
+	return (
+		<>
+			<h1>Anecdote with the most votes</h1>
+			<p>{maxVotes} has {max} votes</p>
+		</>
+	)
+}
 
 // ########### END PART 1 EXERCISE 1.6 - 1.14 ###########
 
@@ -105,27 +167,41 @@ function App() {
     ],
   };
 
+	// Anecdotes
+	const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
   // useState example
   const [counter, setCounter] = useState(0);
   console.log('rendering with counter value', counter);
 
-	// ########### PART 1 EXERCISE 1.6 - 1.14 ###########
-	const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  // ########### PART 1 EXERCISE 1.6 - 1.14 ###########
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+	// Anecdotes
+	const [selected, setSelected] = useState(0);
+	const [votes, setVotes] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0));
 
 
-
-
- // ########### END PART 1 EXERCISE 1.6 - 1.14 ###########
+  // ########### END PART 1 EXERCISE 1.6 - 1.14 ###########
 
   // 2 args: function ref / code, delay
   //setTimeout (() => setCounter(counter + 1), 1000)
   // test
   //console.log('rendering...', counter)
 
-	/*
-	// Functions without parameters
+  /*
+	///////////////// Functions without parameters /////////////////
   const incrementCounter = () => {
 		console.log('increasing, value before', counter)
     setCounter(counter + 1);
@@ -141,8 +217,28 @@ function App() {
   };
 	*/
 
-	const setToValue = (newCounter) => {
-		setCounter(newCounter)
+  const setToValue = (newCounter) => {
+    setCounter(newCounter);
+  };
+
+	const getRandomInt = (min, max) => {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min) + min);
+	}
+
+	const selectedAnecdote = () => {
+		setSelected(getRandomInt(0, anecdotes.length))
+	}
+
+	const calculateVotes = () => {
+		console.log(votes)
+		const votesCopy = [...votes]
+
+		votesCopy[selected] += 1
+		console.log(votesCopy)
+
+		setVotes(votesCopy)
 	}
 
   return (
@@ -164,22 +260,27 @@ function App() {
       <br />
       <br />
       <Button text={'Reset'} handleClick={() => setToValue(0)}></Button>
-			<br />
+      <br />
       <br />
 
-			{/* PART 1 Exercises 1.6 - 1.14 */}
-			<h1>Give Feedback</h1>
-			<Button text={'Good'} handleClick={() => setGood(good + 1)} />
-			<Button text={'Neutral'} handleClick={() => setNeutral(neutral + 1)} />
-			<Button text={'Bad'} handleClick={() => setBad(bad + 1)} />
+      {/* PART 1 Exercises 1.6 - 1.14 */}
+      <h1>Give Feedback</h1>
+      <Button text={'Good'} handleClick={() => setGood(good + 1)} />
+      <Button text={'Neutral'} handleClick={() => setNeutral(neutral + 1)} />
+      <Button text={'Bad'} handleClick={() => setBad(bad + 1)} />
 
-			<h1>Statistics</h1>
-			<DisplayFeedback feedbackName={'good'} feedback={good}  />
-			<DisplayFeedback feedbackName={'neutral'} feedback={neutral}  />
-			<DisplayFeedback feedbackName={'bad'} feedback={bad}  />
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+			<br />
 
+			{/* Anecdotes */}
+			<h1>Anecdotes</h1>
+			<AnecdoteVotes anecdotes={anecdotes} votes={votes} selected={selected} />
+			<Button text={'Vote'} handleClick={calculateVotes}></Button>
+			<Button text={'Next Anecdote'} handleClick={selectedAnecdote} />
 
-
+			<MaxVotes anecdotes={anecdotes} votes={votes} />
+			
     </>
   );
 }
