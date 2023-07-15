f8db4451aae2e47b7ddab07b65d48a721120b2f1
Brandon3522 <77870751+Brandon3522@users.noreply.github.com>
2023-07-15 18:18:26 -0500

    Part 9B: 9.4 - 9.5 formatting

--- part-9/typeScript/exercises/index.ts
+++ part-9/typeScript/exercises/index.ts
@@ -10,35 +10,34 @@ app.get('/hello', (_request, response) => {
 
 // http://localhost:3003/bmi?height=180&weight=72
 app.get('/bmi', (_request, response) => {
-	try {
-		// Throw error if query params are wrong type or missing
-		if ((_request.query.height instanceof String)
-		|| (_request.query.weight instanceof String)) {
-			//console.log(`Error: ${!isNaN(Number(_request.query.height))}, ${typeof _request.query.height}`)
-			throw new Error('Malformatted query params')
-		}
+  try {
+    // Throw error if query params are wrong type or missing
+    if (
+      _request.query.height instanceof String ||
+      _request.query.weight instanceof String
+    ) {
+      //console.log(`Error: ${!isNaN(Number(_request.query.height))}, ${typeof _request.query.height}`)
+      throw new Error('Malformatted query params');
+    }
 
-		const height = Number(_request.query.height);
-		const weight = Number(_request.query.weight);
-		const bmi = calculateBmi(height, weight);
+    const height = Number(_request.query.height);
+    const weight = Number(_request.query.weight);
+    const bmi = calculateBmi(height, weight);
 
-		const result = {
-			weight: weight,
-			height: height,
-			bmi: bmi
-		}
+    const result = {
+      weight: weight,
+      height: height,
+      bmi: bmi,
+    };
 
-		response.send(result).status(200);
-	} catch (error) {
-		if (error instanceof Error) {
-			console.log(error.message);
-			response.status(400).json({error: error.message});
-		}
-	}
-	
-
-
-})
+    response.send(result).status(200);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+      response.status(400).json({ error: error.message });
+    }
+  }
+});
 
 const PORT = 3003;
 
